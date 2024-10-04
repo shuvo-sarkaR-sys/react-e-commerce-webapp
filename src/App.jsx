cfc4937c30add1d14c99504f9b2feeaa5bbe832b
shuvo-sarkaR-sys <md.sirforce@gmail.com>
2024-10-04 12:20:53 +0600

    first change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Home from './Home/Home'
- 
+import Cart from './Cart/Cart'
+import LogIn from './Login/LogIn'
+ import { cartOpt } from './Context/Context'
+import Footer from './Footer/Footer'
+import Search from './components/Search'
+
+import { searchContext } from './Context/Context'
 function App() {
-  
+  const [searchvalue, setSearchValue] = useState("")
+  const [searchBtn, setSearchBtn] = useState('')
+   
+ 
+  const [cart, setCart] = useState([])
   return (
     <>
+    <searchContext.Provider value={{searchvalue, setSearchValue, searchBtn, setSearchBtn}}>
+    <cartOpt.Provider value={{cart, setCart}}>
+    <BrowserRouter>
      <Navbar/>
-     <Hero/>
-      <Home/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+     <Route path='home'   element={<Hero/>}/>
+     <Route path='Cart' element={<Cart/>} />
+     <Route path='LogIn' element={<LogIn/>}/>
+     <Route path='contact' element={<Footer/>} />
+     <Route path='search' element={<Search/>}/>
+      </Routes>
+    </BrowserRouter>
+    </cartOpt.Provider>
+    </searchContext.Provider>
     </>
   )
 }
--- src/assets/asset.js
+++ src/assets/asset.js
@@ -4,6 +4,7 @@ import laptop from './video-lesson.png'
 import gaming from './controller.png'
 import phone from './iphone.png'
 import woman from './profile.png'
+
 import l2 from './l2.webp'
 import l3 from './l3.webp'
 import l4 from './l4.webp'
@@ -24,108 +25,422 @@ import g7 from './g7.webp'
  import m6 from './m6.webp'
  import m7 from './m7.webp'
 
+ import p1 from './product_1.png'
+ 
+ import p3 from './product_3.png'
+ import p4 from './product_4.png'
+ import p5 from './product_5.png'
+ import p6 from './product_6.png'
+ import p7 from './product_7.png'
+ import p8 from './product_8.png'
+ import p9 from './product_9.png'
+ import p10 from './product_10.png'
+ import p11 from './product_11.png'
+ import p12 from './product_12.png'
+
+ import p13 from './product_13.png'
+ import p14 from './product_14.png'
+ import p15 from './product_15.png'
+  
+ import p18 from './product_18.png'
+ import p19 from './product_19.png'
+ import p20 from './product_20.png'
+ import p21 from './product_21.png'
+ import p22 from './product_22.png'
+ import p23 from './product_23.png'
+ import p24 from './product_24.png'
+
+ import p25 from './product_25.png'
+ import p26 from './product_26.png'
+ import p27 from './product_27.png'
+ import p28 from './product_28.png'
+ import p29 from './product_29.png'
+ import p30 from './product_30.png'
+ import p31 from './product_31.png'
+ import p32 from './product_32.png'
+ import p33 from './product_33.png'
+ import p34 from './product_34.png'
+ import p35 from './product_35.png'
+ import p36 from './product_36.png'
 export const product_list = [
     {
         id: 1,
         product_name: 'Asus Laptop',
         product_image: l2, 
-        product_price: '599', 
-    },
-    {
+        product_price: 599, // Number, not a string
+        category: 'Laptop'
+      },
+      {
         id: 2,
         product_name: 'Aser Laptop', 
         product_image: l3,
-        product_price: '699',
-
-    },
-    {
+        product_price: 699,
+        category: 'Laptop'
+      },
+      {
         id: 3,
         product_name: 'Asus tuf Laptop',
         product_image: l4,
-        product_price: '749',
-    },
-    {
+        product_price: 749,
+        category: 'Laptop'
+      },
+      {
         id: 4,
         product_name: 'MSI',
         product_image: l5,
-        product_price: '499',
-    },
-    {
+        product_price: 499,
+        category: 'Laptop'
+      },
+      {
         id: 5, 
         product_name: 'PS5',
         product_image: g1,
-        product_price: '499'
-    },
-    {
+        product_price: 499,
+        category: 'Gaming'
+      },
+      {
         id: 6,
         product_name: 'ps4',
         product_image: g2,
-        product_price: '300'
-    },
-    {
+        product_price: 300,
+        category: 'Gaming'
+      },
+      {
         id: 7,
         product_name: 'PS5 digital edition',
         product_image: g3,
-        product_price: '550'
-    },
-    {
+        product_price: 550,
+        category: 'Gaming'
+      },
+      {
         id: 8,
         product_name: 'Joy stick',
         product_image: g4,
-        product_price: '100'
-    },
-    {
+        product_price: 100,
+        category: 'Gaming'
+      },
+      {
         id: 9, 
         product_name: 'Gaming earphone',
         product_image: g5,
-        product_price: '120'
-    },
-    {
+        product_price: 120,
+        category: 'Gaming'
+      },
+      {
         id: 10, 
         product_name: 'Xbox',
         product_image: g6,
-        product_price: '350'
-    },
-    {
+        product_price: 350,
+        category: 'Gaming'
+      },
+      {
         id: 11,
         product_name: 'Xbox 2.0',
         product_image: g7,
-        product_price: '370',
-    },
-    {
+        product_price: 370,
+        category: 'Gaming'
+      },
+      {
         id: 12,
         product_name: 'Iphone 16',
         product_image: m1,
-        product_price: '899',
-        
-
-    },
-    {
+        product_price: 899,
+        category: 'Phone'
+      },
+      {
         id: 13,
         product_name: 'Iphone 16pro max',
         product_image: m6,
-        product_price: '1399',
-
-    },
-    {
+        product_price: 1399,
+        category: 'Phone'
+      },
+      {
         id: 14,
         product_name: 'Iphone 13pro',
         product_image: m7,
-        product_price: '699',
-    },
-    {
+        product_price: 699,
+        category: 'Phone'
+      },
+      {
         id: 15,
         product_name: 'MOto',
         product_image: m2,
-    product_price: 140,
-    },
-    {
+        product_price: 140,
+        category: 'Phone'
+      },
+      {
         id: 16,
         product_name: 'samsung',
         product_image: m3,
         product_price: 799,
+        category: 'Phone'
+      },
+      {
+        id: 17,
+        product_name: 'samsung s24ultra',
+        product_image: m4,
+        product_price: 1299,
+        category: 'Phone'
+      },
+      {
+        id: 18, 
+        product_name: 'samsung s24fe',
+        product_image: m5,
+        product_price: 499,
+        category: 'Phone'
+      },
+      {
+        id: 19,
+        product_name: "women cloth",
+        product_image: p1,
+        product_price: 100,
+        category: 'Women'
+      },
+      {
+        id: 20,
+        product_name: "women cloth",
+        product_image: p3,
+        product_price: 110,
+        category: 'Women'
+      },
+      {
+        id: 21,
+        product_name: "women cloth",
+        product_image: p4,
+        product_price: 90,
+        category: 'Women'
+      },
+      {
+        id: 22,
+        product_name: "women cloth",
+        product_image: p5,
+        product_price: 120,
+        category: 'Women'
+      },
+      {
+        id: 23,
+        product_name: "women cloth",
+        product_image: p6,
+        product_price: 130,
+        category: 'Women'
+      },
+      {
+        id: 24,
+        product_name: "women cloth",
+        product_image: p7,
+        product_price: 140,
+        category: 'Women'
+      },
+      {
+        id: 25,
+        product_name: "women cloth",
+        product_image: p8,
+        product_price: 110,
+        category: 'Women'
+      },
+      {
+        id: 26,
+        product_name: "women cloth",
+        product_image: p9,
+        product_price: 150,
+        category: 'Women'
+      },
+      {
+        id: 27,
+        product_name: "women cloth",
+        product_image: p10,
+        product_price: 105,
+        category: 'Women'
+      },
+      {
+        id: 28,
+        product_name: "women cloth",
+        product_image: p11,
+        product_price: 99,
+        category: 'Women'
+      },
+      {
+        id: 29,
+        product_name: "women cloth",
+        product_image: p12,
+        product_price: 120,
+        category: 'Women'
+      },
+      {
+        id: 30,
+        product_name: "man jacket",
+        product_image: p13,
+        product_price: 130,
+        category: 'Men'
+      },
+      {
+        id: 31,
+        product_name: "man cloth",
+        product_image: p14,
+        product_price: 140,
+        category: 'Men'
+      },
+      {
+        id: 32,
+        product_name: "man cloth",
+        product_image: p15,
+        product_price: 150,
+        category: 'Men'
+      },
+      {
+        id: 33,
+        product_name: "man cloth",
+        product_image: p18,
+        product_price: 150,
+        category: 'Men'
+      },
+      {
+        id: 34,
+        product_name: "man cloth",
+        product_image: p19,
+        product_price: 190,
+        category: 'Men'
+      },
+      {
+        id: 35,
+        product_name: "man cloth",
+        product_image: p20,
+        product_price: 200,
+        category: 'Men'
+      },
+      {
+        id: 36,
+        product_name: "man cloth",
+        product_image: p21,
+        product_price: 210,
+        category: 'Men'
+      },
+      {
+        id: 37,
+        product_name: "man cloth",
+        product_image: p22,
+        product_price: 220,
+        category: 'Men'
+      },
+      {
+        id: 38,
+        product_name: "man cloth",
+        product_image: p23,
+        product_price: 230,
+        category: 'Men'
+      },
+      {
+        id: 39,
+        product_name: "man cloth",
+        product_image: p24,
+        product_price: 240,
+        category: 'Men'
+      },
+      {
+        id: 40,
+        product_name: "boy cloth",
+        product_image: p25,
+        product_price: 250,
+        category: 'Boy'
+      },
+      {
+        id: 41,
+        product_name: "Boy cloth",
+        product_image: p26,
+        product_price: 260,
+        category: 'Boy'
+      },
+      {
+        id: 42,
+        product_name: "Boy cloth",
+        product_image: p27,
+        product_price: 270,
+        category: 'Boy'
+      },
+      {
+        id: 43,
+        product_name: "Boy cloth",
+        product_image: p28,
+        product_price: 280,
+        category: 'Boy'
+      },
+      {
+        id: 44,
+        product_name: "Boy cloth",
+        product_image: p29,
+        product_price: 290,
+        category: 'Boy'
+      },
+      {
+        id: 45,
+        product_name: "Boy cloth",
+        product_image: p30,
+        product_price: 290,
+        category: 'Boy'
+      },
+      {
+        id: 46,
+        product_name: "Boy cloth",
+        product_image: p31,
+        product_price: 290,
+        category: 'Boy'
+      },
+    
+    {
+        id: 47,
+        product_name: "Boy cloth",
+        product_image: p32,
+        product_price: 290,
+        category: 'Boy'
+
+    },
+    
+    {
+        id: 48,
+        product_name: "Boy cloth",
+        product_image: p33,
+        product_price: 190,
+        category: 'Boy'
+
     }
- 
+    ,
+    
+    {
+        id: 49,
+        product_name: "Boy cloth",
+        product_image: p34,
+        product_price: 150,
+        category: 'Boy'
+
+    },
+    
+    {
+        id: 50,
+        product_name: "Boy cloth",
+        product_image: p35,
+        product_price: 120,
+        category: 'Boy'
+
+    },
+    
+    {
+        id: 51,
+        product_name: "Boy cloth",
+        product_image: p36,
+        product_price: 130,
+        category: 'Boy'
+
+    },
+    
+    {
+        id: 48,
+        product_name: "Boy cloth",
+        product_image: p31,
+        product_price: 190,
+        category: 'Boy'
+
+    }
+
 
     
 ]
@@ -144,7 +459,7 @@ export const Category_list = [
     },
     {
         id: 3,
-        category_name: 'boy',
+        category_name: 'Boy',
         category_image: boy
     },
     {
@@ -154,9 +469,13 @@ export const Category_list = [
     },
     {
         id: 5,
-        category_name: 'gaming',
+        category_name: 'Gaming',
         category_image: gaming
     }, 
-    
+    {
+        id: 6,
+        category_name: 'Phone',
+        category_image: phone
+    }
 
 ]
